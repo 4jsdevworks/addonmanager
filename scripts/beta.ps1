7aec4bf41dd6f577aab7325d10a2ab6657ed4fdd
John Rogers <john.rogers@becu.org>
2020-09-21 09:04:38 -0700

    working downloader for elvui

--- scripts/beta.ps1
+++ scripts/beta.ps1
@@ -2,7 +2,20 @@ Clear-Host
 
 & .\install-modules.ps1
 
-Write-Verbose -Verbose "downloading elvui"
-get-addon -name "ElvUI" -repository "https://git.tukui.org/elvui/elvui.git"
+Write-Verbose -Verbose "Creating temp folder"
+Invoke-CreateAddonsFolder
+
+$Config = Get-Config -File "$PSScriptRoot\config\beta.json"
+foreach($addon in $Config.addons)
+{
+    Get-Addon -Name $addon.name -Branch $addon.branch -Repository $addon.repository
+
+    foreach($folder in $addon.folders)
+    {
+        Copy-FolderToAddons -Name $addon.name -Folder $folder -Addons $config.folder
+    }
+}
+
+Write-Verbose -Verbose "Script Complete"
 
 Set-Location $PSScriptRoot
--- scripts/modules/addons.psm1
+++ scripts/modules/addons.psm1
@@ -1,28 +1,56 @@
 using namespace System.IO
 
-function Get-Addon([string] $name, [string] $repository) {
-    [string] $downloads = "$env:TEMP\addons"
-    Remove-IfExists $downloads
+[string] $downloads = "$env:TEMP\addons"
 
-    Write-Verbose -Verbose "Creating download folder"
-    New-Item -Path $downloads -ItemType Directory | Out-Null
-    Set-Location $downloads
-    
-    git clone $repository $name
-
-    Set-Location $name
-
-    Remove-IfExists ".gitlab"
-    Remove-IfExists ".gitignore"
-    Remove-IfExists "changelog.md"
-
-    Get-ChildItem
+function Get-Config([string] $File) {
+    return Get-Content $File | ConvertFrom-Json
 }
 
-function Remove-IfExists([string] $name) {
-    if (Test-Path -Path $name)
+function Invoke-CreateAddonsFolder() {
+    if ((Test-Path -Path $downloads) -eq $false)
     {
-        Write-Verbose -Verbose "Removing $name"
-        Remove-Item $name -Recurse -Force | Out-Null
+        Write-Verbose -Verbose "Creating download folder $downloads"
+        New-Item -Path $downloads -ItemType Directory | Out-Null
+    }
+
+    Write-Verbose -Verbose "$downloads is now the current directory"
+    Set-Location $downloads
+}
+
+function Get-Addon([string] $Name, [string] $Branch, [string] $Repository) {
+    if ((Test-Path -Path $Name) -eq $false)
+    {
+        Write-Verbose -Verbose "Cloning Repo $Repository into $downloads\$name"
+        git clone $Repository $Name
+    }
+
+    
+    Write-Verbose -Verbose "$downloads\$name is now the current directory"
+    Set-Location $Name
+
+    Write-Verbose -Verbose "Switching to the $Branch branch in $Repository"
+    git checkout $Branch
+    git pull
+}
+
+
+function Copy-FolderToAddons([string] $Name, [string] $Folder, [string] $Addons) {
+    $source = "$downloads\$Name\$Folder"
+    $destination = "$Addons\$Folder"
+
+    Remove-IfExists $destination
+
+    Write-Verbose -Verbose "Copying $Name to $Addons"
+    Copy-Item -Path $source -Destination $destination -Recurse
+}
+
+
+function Remove-IfExists([string] $Name) {
+    if (Test-Path -Path $Name)
+    {
+        Write-Verbose -Verbose "Removing $Name"
+        Remove-Item $Name -Recurse -Force | Out-Null
     }
 }
+
+Export-ModuleMember -Function '*'
